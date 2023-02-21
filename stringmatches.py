67cf2c9fe10c38e1bb8fa43cd6a7c40d7999b89d
JuanA072 <113403810+JuanA072@users.noreply.github.com>
2023-02-20 19:38:43 -0500

    Add files via upload

---
+++ stringmatches.py
@@ -0,0 +1,16 @@
+import re
+
+def is_integer_literal(s):
+    pattern = r'^[+-]?\d+$'
+    return bool(re.match(pattern, s))
+
+# Example cases:
+print(is_integer_literal("123"))    #True
+print(is_integer_literal("+123"))   #rue
+print(is_integer_literal("-123"))   #True
+print(is_integer_literal("0"))      #True
+print(is_integer_literal("007"))    #True
+print(is_integer_literal("123.0"))  #False
+print(is_integer_literal("123a"))   #Fase
+
+#check if the string matches the pattern of an integer literal, which is an optional sign (+/-) followed by one or more digits (0-9). The ^ and $ anchors are used to match the entire string. If the match is successful, the function returns True, otherwise it returns False.
